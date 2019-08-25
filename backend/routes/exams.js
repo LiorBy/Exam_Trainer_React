94ac1ea9e4bc23cbd9820ae256430a1acb6de621
Roy Prager <roy.prager@argus-sec.com>
2019-08-25 20:54:20 +0300

    add getById,  edit, delete exam functionality

--- backend/routes/exams.js
+++ backend/routes/exams.js
@@ -17,6 +17,56 @@ router.get("/", (req, res) => {
   });
 });
 
+//get exam by id
+router.get("/:id", (req, res) => {
+  Exam.findById(req.params.id, function(err, exam) {
+    if (err) {
+      res.status(500).send("Error");
+    } else if (!exam) {
+      res.status(401).send("Exam not found");
+    } else {
+      res.status(200).json({
+        exam: exam
+      });
+    }
+  });
+});
+
+//edit exam by id
+router.put("/:id", (req, res) => {
+  const { name, course, year, semester, lecturer, raw_text } = req.body;
+  const newExam = {};
+  newExam.name = req.body.name;
+  newExam.course = req.body.course;
+  newExam.year = req.body.year;
+  newExam.semester = req.body.semester;
+  newExam.lecturer = req.body.lecturer;
+  newExam.raw_text = req.body.raw_text;
+
+  Exam.findByIdAndUpdate({ _id: req.params.id }, newExam, function(err, exam) {
+    if (err) {
+      res.status(500).send(err);
+    } else if (!exam) {
+      res.status(401).send("Exam not found");
+    } else {
+      res.status(200).json({
+        exam: exam
+      });
+    }
+  });
+});
+
+//delete exam by id
+router.delete("/:id", (req, res) => {
+  Exam.findByIdAndDelete({ _id: req.params.id }, function(err) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send("Exam deleted");
+    }
+  });
+});
+
 //insert new exam
 router.post("/", (req, res) => {
   const { name, course, year, semester, lecturer, raw_text } = req.body;
